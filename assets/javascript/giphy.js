56da55d2a26cb9f86b5b1107785a6c82f26baaae
unknown <moogieee@ODSPR10234W45.odsdai.netdai.com>
2017-07-19 15:51:26 -0700

    created buttons for each animal, displayed gifs on page

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,90 @@
+var animals = ["dog", "cat", "quokka", "sloth", "goat", "kangaroo", "squirrel", "otter", "hedgehog", "hamster"]
+
+function displayAnimalGif() {
+	var animal = $(this).attr("data-name");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + " &api_key=dc6zaTOxFJmzC";
+
+	// create ajax call
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
+		console.log(response.data)
+		// save results as a variable
+		var results = response.data;
+
+		for (var i = 0; results.length; i++) {
+		// create a div that holds the GIFs
+			var animalGifDiv = $("<div class='animalgifs'>");
+			var showAnimal = $("<img>");
+
+			// retrieve the gif still
+				showAnimal.attr("data-still", results[i].images.fixed_height_still.url);
+			// retrieve animated gif
+				showAnimal.attr("data-animate", results[i].images.fixed_height.url);
+
+			animalGifDiv.append(showAnimal);
+
+			showAnimal.attr("src", results[i].images.fixed_height_still.url);
+			$("#animals-view").prepend(animalGifDiv);
+
+		}
+
+	});
+}
+
+// animate the gif on click
+$("#animals-view").on("click", "showAnimal", function() {
+	var stillURL = $(this).attr("data-still");
+	var animateURL = $(this).attr("data-animate");
+	var currentImage = $(this).attr("src");
+
+	if(currentImage === stillURL) {
+		$(this).attr("src", animateURL);
+	} else {
+		$(this).attr("src", stillURL);
+	}
+
+});
+
+// function for displaying animal gifs
+function renderButtons() {
+
+	// delete the animals prior to adding new animals
+	$("#buttons-view").empty();
+
+	// loop through the array of animals
+	for (var i = 0; i < animals.length; i++) {
+
+		// generate a button for each animal in the array
+		var animalButton = $("<button>");
+		// add a class to the button
+		animalButton.addClass("animal");
+		// add a data-attribute
+		animalButton.attr("data-name", animals[i]);
+		// put button text
+		animalButton.text(animals[i]);
+		// add button to the buttons-view div
+		$("#buttons-view").append(animalButton);
+	}
+}
+
+// function that handles events when the add gif button is clicked 
+	$("#add-animal").on("click", function(event) {
+		event.preventDefault();
+
+		// grab input from the textbox 
+		var animal = $("#animal-input").val().trim();
+
+		// the animal from the textbox is added to array
+		animals.push(animal);
+
+		//call the renderButtons which handles the processing of the animal array
+		renderButtons();
+	});
+
+	// add click event to all elements with a class of "animal"
+	$(document).on("click", ".animal", displayAnimalGif);
+
+	// call the renderButtons function to display the initial buttons
+	renderButtons();
